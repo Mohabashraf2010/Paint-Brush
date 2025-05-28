381d2a0c8c3367d53e7c0ee71a41804e5d1187a1
Mohabashraf2010 <67844934+Mohabashraf2010@users.noreply.github.com>
2025-05-28 15:26:13 +0300

    Add files via upload

---
+++ paint brush/paint brush/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace paint_brush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineToolStripMenuItem,
+            this.rectangleToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lineToolStripMenuItem.Text = "Line";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rectangleToolStripMenuItem.Text = "Rectangle";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.rectangleToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.blackToolStripMenuItem,
+            this.greenToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.blackToolStripMenuItem.Text = "Black";
+            this.blackToolStripMenuItem.Click += new System.EventHandler(this.blackToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ paint brush/paint brush/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint_brush
+{
+    public partial class Form1 : Form
+    {
+        List<shape> shapes = new List<shape>();
+        shape temp = new shape();
+        int shapetype = 0; // 1 for line, 2 for rectangle
+        bool isDrawing = false; // flag to indicate if the mouse is being dragged
+
+        Color currcolor = Color.Black; // current color of the shape
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+
+            if (shapetype == 1) // line
+            {
+                temp.x1 = e.X; // starting point of the line
+                temp.y1 = e.Y;
+                temp.x2 = e.X; // ending point of the line
+                temp.y2 = e.Y;
+            }
+            else if (shapetype == 2) // rectangle
+            {
+                temp.x1 = e.X; // starting point of the rectangle
+                temp.y1 = e.Y;
+                temp.x2 = e.X; // ending point of the rectangle
+                temp.y2 = e.Y;
+            }
+        }
+
+        class shape
+        {
+            public Color color = Color.Black;
+            public int x1, y1, x2, y2; // starting and ending points of the shape
+        }
+        class Line : shape
+        {
+            
+        }
+
+        class myrectangle : shape
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (isDrawing)
+            {
+                Graphics beta3 = this.CreateGraphics();  //gives me a graphics handle for the form
+                Pen pen = new Pen(currcolor);
+                Pen erasePen = new Pen(this.BackColor); // create a pen with the background color
+                if (shapetype == 1) // line
+                {
+                    beta3.DrawLine(erasePen, temp.x1, temp.y1, temp.x2, temp.y2); // erase the previous line
+                    temp.x2 = e.X; // update the ending point of the line
+                    temp.y2 = e.Y;
+                    beta3.DrawLine(pen, temp.x1, temp.y1, temp.x2, temp.y2); // draw the new line
+                }
+                else if (shapetype == 2) // rectangle
+                {
+                    beta3.DrawRectangle(erasePen, Math.Min(temp.x1, e.X), Math.Min(temp.y1, e.Y), Math.Abs(temp.x1 - e.X), Math.Abs(temp.y1 - e.Y)); // erase the previous rectangle
+                                                                                                                                                     // Instead of erasing and redrawing the rectangle on the form's surface, simply invalidate the form to trigger a repaint.
+                                                                                                                                                     // Replace the selected lines in Form1_MouseMove with:
+                    if (isDrawing)
+                    {
+                        temp.x2 = e.X;
+                        temp.y2 = e.Y;
+                        this.Invalidate(); // triggers Form1_Paint, which calls restore()
+                    }
+                    beta3.DrawRectangle(pen, temp.x1, temp.y1, temp.x2 - temp.x1, temp.y2 - temp.y1); // draw the new rectangle
+                }
+            }
+            restore(); // restore the shapes on the form
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false; // stop drawing
+            Graphics beta3 = this.CreateGraphics();  //gives me a graphics handle for the form
+            Pen pen = new Pen(Color.Black);
+            beta3.DrawLine(pen, temp.x1, temp.y1, temp.x2, temp.y2);
+
+            if (shapetype == 1) // line
+            {
+                Line newLine = new Line();
+                newLine.x1 = temp.x1;
+                newLine.y1 = temp.y1;
+                newLine.x2 = temp.x2;
+                newLine.y2 = temp.y2;
+                newLine.color = currcolor;
+                shapes.Add(newLine);
+            }
+            else if (shapetype == 2) // rectangle
+            {
+                myrectangle newRectangle = new myrectangle();
+                newRectangle.x1 = temp.x1;
+                newRectangle.y1 = temp.y1;
+                newRectangle.x2 = temp.x2;
+                newRectangle.y2 = temp.y2;
+                newRectangle.color = currcolor;
+                shapes.Add(newRectangle);
+            }
+        }
+
+        void restore()
+        {
+            Graphics beta3 = this.CreateGraphics();  //gives me a graphics handle for the form
+            foreach (shape x in shapes)
+            {
+                Pen pen1 = new Pen(x.color);
+
+                if (x is Line)
+                {
+                    Line line = (Line)x;
+                    beta3.DrawLine(pen1, line.x1, line.y1, line.x2, line.y2);
+                }
+                else if (x is myrectangle)
+                {
+                    myrectangle rectangle = (myrectangle)x;
+                    beta3.DrawRectangle(pen1, Math.Min(rectangle.x1, rectangle.x2), Math.Min(rectangle.y1, rectangle.y2), Math.Abs(rectangle.x1 - rectangle.x2), Math.Abs(rectangle.y1 - rectangle.y2));
+                }
+
+                pen1.Dispose();
+            }
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            restore();
+        }
+
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shapetype = 1; // set the shape type to line
+        }
+
+        private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shapetype = 2;
+            restore();
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             currcolor = Color.Red; // set the current color to red
+        }
+
+        private void blackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             currcolor = Color.Black;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             currcolor = Color.Green;
+        }
+
+        // Replace the incorrect block in saveToolStripMenuItem_Click with the following:
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter myFile = new System.IO.StreamWriter("c:/test/shapes.txt");
+            foreach (shape x in shapes)
+            {
+                if (x is Line)
+                {
+                    myFile.WriteLine("Line");
+                }
+                else if (x is myrectangle)
+                {
+                    myFile.WriteLine("Rectangle");
+                }
+            }
+            myFile.Close();
+        }
+    }
+}
